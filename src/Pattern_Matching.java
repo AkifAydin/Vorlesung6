9bc27c6baec93afc9ad49f3f017a3151a3735f82
Akif Aydin <akifnidya@hotmail.de>
2021-08-27 20:44:16 +0200

    Initial commit

---
+++ src/Pattern_Matching.java
@@ -0,0 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Pattern_Matching {
+    Pattern pattern = Pattern.compile("'.*'");
+    Matcher matcher = pattern.matcher("'Moin Folks");
+    boolean doesMatch = matcher.matches();
+
+    
+
+    public static void main(String[] args) {
+
+    }
+}
