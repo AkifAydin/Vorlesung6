23c34a0c05835f890c3ba8fe5185e5d5636a6ea7
Akif Aydin <akifnidya@hotmail.de>
2021-08-28 12:45:00 +0200

    Onur

--- src/Pattern_Matching.java
+++ src/Pattern_Matching.java
@@ -2,9 +2,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Pattern_Matching {
-    Pattern pattern = Pattern.compile("'.*'");
-    Matcher matcher = pattern.matcher("'Moin Folks");
-    boolean doesMatch = matcher.matches();
-    public static void main(String[] args) {
+
+    private static void capturingGroups1() {
+        String regex = "((\\d+?)([a-zA-Z.]+(\\d+?)))";
+        String str = "123aaaaXX.klaa756";
+        Pattern pt = Pattern.compile(regex);
+        Matcher matcher = pt.matcher(str);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group(0));
+            System.out.println(matcher.group(1));
+            System.out.println(matcher.group(2));
+            System.out.println(matcher.group(3));
+            System.out.println(matcher.group(4));
+            System.out.println(matcher.start());
+            System.out.println(matcher.end());
+        }
     }
+
+    public static void main(String[] args) {
+        capturingGroups1();
+
+    }
+
+
 }
